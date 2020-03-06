9230c06052efe344b6901b1f6ca7e0cbdb38ecc4
Karim <medkarimrdi@gmail.com>
2020-03-06 17:01:14 +0800

    remove default comments

--- cmd/subscribe.go
+++ cmd/subscribe.go
@@ -188,16 +188,6 @@ var subscribeCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(subscribeCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// subscribeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// subscribeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	subscribeCmd.Flags().StringP("prefix", "", "", "subscribe request prefix")
 	subscribeCmd.Flags().StringSliceP("path", "", []string{"/"}, "subscribe request paths")
 	subscribeCmd.Flags().Int32P("qos", "q", 20, "qos marking")
