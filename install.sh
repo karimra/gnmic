d7702163fdce6b2dba66f3e79eb4a71aba097b9a
dodin.roman@gmail.com <dodin.roman@gmail.com>
2020-04-22 23:53:05 +0200

    skipped bin download if the same version installed

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 TAR_PREFIX=gnmiClient
 PLATFORM=$(uname)
 ARCH=$(uname -m)
+INSTALLED_VERSION=$(gnmiClient version | grep version | awk '{print $3}') || ""
 LATEST_URL=$(curl -s https://github.com/karimra/gnmiClient/releases/latest | cut -d '"' -f 2)
 LATEST_TAG=$(echo "${LATEST_URL: -6}")
 TAG_WO_VER=$(echo "${LATEST_URL: -6}" | cut -c 2-)
+if [ "$INSTALLED_VERSION" == "$TAG_WO_VER" ]; then
+    echo "You have the latest gnmiClient version installed: $INSTALLED_VERSION"
+    exit 0
+fi
 FNAME="${TAR_PREFIX}_${TAG_WO_VER}_${PLATFORM}_${ARCH}.tar.gz"
 echo "Downloading $FNAME..."
 (cd /tmp && curl -ksLO https://github.com/karimra/gnmiClient/releases/download/"$LATEST_TAG"/"$FNAME")
