7d3795fd92e16019af61e05dc7227a0d1e545f6f
Radhouani Karim <karim.radhouani@nokia.com>
2020-06-18 17:34:56 +0800

    add stan output

--- outputs/all/all.go
+++ outputs/all/all.go
@@ -3,4 +3,5 @@ package all
 import (
 	_ "github.com/karimra/gnmiClient/outputs/file"
 	_ "github.com/karimra/gnmiClient/outputs/nats_output"
+	_ "github.com/karimra/gnmiClient/outputs/stan_output"
 )
--- outputs/stan_output/stan_output.go
+++ outputs/stan_output/stan_output.go
@@ -1,1 +1,107 @@
 package stan_output
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/mitchellh/mapstructure"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
+)
+
+const (
+	stanDefaultTimeout      = 10
+	stanDefaultPingInterval = 5
+	stanDefaultPingRetry    = 2
+)
+
+// StanOutput //
+type StanOutput struct {
+	Cfg      *Config
+	conn     stan.Conn
+	stopChan chan struct{}
+}
+
+// Config //
+type Config struct {
+	Name         string
+	Address      string
+	Subject      string
+	Username     string
+	Password     string
+	ClusterName  string
+	Timeout      int
+	PingInterval int
+	PingRetry    int
+}
+
+// Initialize //
+func (s *StanOutput) Initialize(cfg map[string]interface{}) error {
+	err := mapstructure.Decode(cfg, s.Cfg)
+	if err != nil {
+		return err
+	}
+	if s.Cfg.Name == "" {
+		s.Cfg.Name = "gnmiclient-" + uuid.New().String()
+	}
+	if s.Cfg.ClusterName == "" {
+		return fmt.Errorf("clusterName is mandatory")
+	}
+	s.conn, err = createSTANConn(s.Cfg)
+	if err != nil {
+		return err
+	}
+	s.stopChan = make(chan struct{})
+	return nil
+}
+
+// Write //
+func (s *StanOutput) Write(b []byte) {
+	err := s.conn.Publish(s.Cfg.Subject, b)
+	if err != nil {
+		log.Printf("failed to write to stan subject '%s': %v", s.Cfg.Subject, err)
+		return
+	}
+}
+
+// Close //
+func (s *StanOutput) Close() error {
+	close(s.stopChan)
+	return s.conn.Close()
+}
+
+func createSTANConn(c *Config) (stan.Conn, error) {
+	if c.Timeout == 0 {
+		c.Timeout = stanDefaultTimeout
+	}
+	if c.PingInterval == 0 {
+		c.PingInterval = stanDefaultPingInterval
+	}
+	if c.PingRetry == 0 {
+		c.PingRetry = stanDefaultPingRetry
+	}
+	opts := []nats.Option{
+		nats.Name(c.Name),
+		// nats.Timeout(time.Duration(c.Timeout) * time.Second),
+		// nats.PingInterval(time.Duration(c.PingInterval) * time.Second),
+		// nats.MaxPingsOutstanding(c.PingRetry),
+	}
+	if c.Username != "" && c.Password != "" {
+		opts = append(opts, nats.UserInfo(c.Username, c.Password))
+	}
+	nc, err := nats.Connect(c.Address, opts...)
+	if err != nil {
+		return nil, err
+	}
+	sc, err := stan.Connect(c.ClusterName, c.Name,
+		stan.NatsConn(nc),
+		stan.Pings(c.PingInterval, c.PingRetry),
+		stan.SetConnectionLostHandler(func(_ stan.Conn, err error) {
+			log.Fatalf("STAN connection lost, reason: %v", err)
+		}))
+	if err != nil {
+		return nil, fmt.Errorf("Can't connect to %s: %v", c.Address, err)
+	}
+	return sc, nil
+}
