5b81c1311912054d23d406066c3bf5e96bc9cf5a
Radhouani Karim <karim.radhouani@nokia.com>
2020-06-18 17:37:26 +0800

    add kafka output

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/karimra/gnmiClient
 go 1.13
 
 require (
+	github.com/Shopify/sarama v1.26.4
 	github.com/gogo/protobuf v1.2.1
 	github.com/golang/protobuf v1.4.2
 	github.com/google/gnxi v0.0.0-20191016182648-6697a080bc2d
@@ -13,12 +14,14 @@ require (
 	github.com/mitchellh/mapstructure v1.1.2
 	github.com/nats-io/nats-server/v2 v2.1.7 // indirect
 	github.com/nats-io/nats.go v1.10.0
+	github.com/nats-io/stan.go v0.6.0
 	github.com/openconfig/gnmi v0.0.0-20200508230933-d19cebf5e7be
 	github.com/openconfig/goyang v0.0.0-20200309174518-a00bece872fc
 	github.com/spf13/cobra v0.0.6
+	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.6.2
 	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59
 	google.golang.org/grpc v1.29.1
 	google.golang.org/protobuf v1.23.0
-	gopkg.in/yaml.v2 v2.2.4
+	gopkg.in/yaml.v2 v2.2.8
 )
--- outputs/kafka_output/kafka_output.go
+++ outputs/kafka_output/kafka_output.go
@@ -1,1 +1,86 @@
 package kafka_output
+
+import (
+	"log"
+	"strings"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/karimra/gnmiClient/outputs"
+	"github.com/mitchellh/mapstructure"
+)
+
+const (
+	defaultKafkaMaxRetry = 2
+	defaultKafkaTimeout  = 5
+	defaultKafkaTopic    = "telemetry"
+)
+
+func init() {
+	outputs.Register("kafka", func() outputs.Output {
+		return &KafkaOutput{
+			Cfg: &Config{},
+		}
+	})
+}
+
+// KafkaOutput //
+type KafkaOutput struct {
+	Cfg      *Config
+	producer sarama.SyncProducer
+	stopChan chan struct{}
+}
+
+// Config //
+type Config struct {
+	Address  string
+	Topic    string
+	MaxRetry int
+	Timeout  int
+}
+
+// Initialize /
+func (k *KafkaOutput) Initialize(cfg map[string]interface{}) error {
+	err := mapstructure.Decode(cfg, k.Cfg)
+	if err != nil {
+		return err
+	}
+	if k.Cfg.Topic == "" {
+		k.Cfg.Topic = defaultKafkaTopic
+	}
+	if k.Cfg.MaxRetry == 0 {
+		k.Cfg.MaxRetry = defaultKafkaMaxRetry
+	}
+	if k.Cfg.Timeout == 0 {
+		k.Cfg.Timeout = defaultKafkaTimeout
+	}
+	config := sarama.NewConfig()
+	config.Producer.Retry.Max = k.Cfg.MaxRetry
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Return.Successes = true
+	config.Producer.Timeout = time.Duration(k.Cfg.Timeout) * time.Second
+
+	k.producer, err = sarama.NewSyncProducer(strings.Split(k.Cfg.Address, ","), config)
+	return err
+}
+
+// Write //
+func (k *KafkaOutput) Write(b []byte) {
+	if len(b) == 0 {
+		return
+	}
+	msg := &sarama.ProducerMessage{
+		Topic: k.Cfg.Topic,
+		Value: sarama.ByteEncoder(b),
+	}
+	_, _, err := k.producer.SendMessage(msg)
+	if err != nil {
+		log.Printf("failed to send a kafka msg to topic '%s': %v", k.Cfg.Topic, err)
+	}
+}
+
+// Close //
+func (k *KafkaOutput) Close() error {
+	close(k.stopChan)
+	return k.producer.Close()
+}
