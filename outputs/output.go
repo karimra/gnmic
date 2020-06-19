3f4a1e5de1eb1ed13676f2b96de86f95c00dbb42
Radhouani Karim <karim.radhouani@nokia.com>
2020-06-19 17:35:18 +0800

    add Metrics() to output interface{}

--- outputs/output.go
+++ outputs/output.go
@@ -1,11 +1,16 @@
 package outputs
 
-import "log"
+import (
+	"log"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 type Output interface {
 	Init(map[string]interface{}, *log.Logger) error
 	Write([]byte)
 	Close() error
+	Metrics() []prometheus.Collector
 }
 type Initializer func() Output
 
