75da12563852f76225b412505c3a23a6f592b5db
Radhouani Karim <karim.radhouani@nokia.com>
2020-06-23 09:31:29 +0800

    rename file metrics

--- outputs/file/stats.go
+++ outputs/file/stats.go
@@ -2,15 +2,15 @@ package file
 
 import "github.com/prometheus/client_golang/prometheus"
 
-var NumberOfBytes = prometheus.NewCounterVec(prometheus.CounterOpts{
-	Name: "number_of_bytes",
+var NumberOfWrittenBytes = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "number_of_written_bytes_total",
 	Help: "Number of bytes written to file",
 }, []string{"file_name"})
 var NumberOfReceivedMsgs = prometheus.NewCounterVec(prometheus.CounterOpts{
-	Name: "number_of_received_messages",
+	Name: "number_of_received_messages_total",
 	Help: "Number of messages received by file output",
 }, []string{"file_name"})
 var NumberOfWrittenMsgs = prometheus.NewCounterVec(prometheus.CounterOpts{
-	Name: "number_of_written_messages",
+	Name: "number_of_written_messages_total",
 	Help: "Number of messages written to file",
 }, []string{"file_name"})
