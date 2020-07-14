86aa516d6e3b0de6e1fdd2b89ae404734d837513
Radhouani Karim <karim.radhouani@nokia.com>
2020-07-14 17:03:10 +0800

    update/fix tests

--- collector/helpers_test.go
+++ collector/helpers_test.go
@@ -1,6 +1,7 @@
 package collector
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
 	"testing"
@@ -10,19 +11,69 @@ import (
 
 var prefixSet = map[string]*gnmi.Path{
 	"": nil,
-	"target:::/e1/e2": {
+	"target%%%origin:/e1/e2": {
+		Origin: "origin",
 		Target: "target",
 		Elem: []*gnmi.PathElem{
 			{Name: "e1"},
 			{Name: "e2"},
 		},
 	},
-	":::/e1": {
+	"/e1": {
 		Elem: []*gnmi.PathElem{
 			{Name: "e1"},
 		},
 	},
-	":::/e1/e2[k=v]": {
+	"/e1/e2[k=v]": {
+		Elem: []*gnmi.PathElem{
+			{Name: "e1"},
+			{Name: "e2",
+				Key: map[string]string{
+					"k": "v",
+				}},
+		},
+	},
+}
+
+var pathsSet = map[string]*gnmi.Path{
+	"/": {},
+	"origin:e1/e2": {
+		Origin: "origin",
+		Elem: []*gnmi.PathElem{
+			{Name: "e1"},
+			{Name: "e2"},
+		},
+	},
+	"origin:/e1/e2": {
+		Origin: "origin",
+		Elem: []*gnmi.PathElem{
+			{Name: "e1"},
+			{Name: "e2"},
+		},
+	},
+	"origin:": {Origin: "origin"},
+	"e": {
+		Elem: []*gnmi.PathElem{
+			{Name: "e"},
+		},
+	},
+	"/e1/e2": {
+		Elem: []*gnmi.PathElem{
+			{Name: "e1"},
+			{Name: "e2"},
+		},
+	},
+	"/e1/e2[k=v]": {
+		Elem: []*gnmi.PathElem{
+			{Name: "e1"},
+			{Name: "e2",
+				Key: map[string]string{
+					"k": "v",
+				}},
+		},
+	},
+	"origin:/e1/e2[k=v]": {
+		Origin: "origin",
 		Elem: []*gnmi.PathElem{
 			{Name: "e1"},
 			{Name: "e2",
@@ -36,17 +87,33 @@ var prefixSet = map[string]*gnmi.Path{
 func TestCreatePrefix(t *testing.T) {
 	var target, prefix string
 	for e, p := range prefixSet {
-		val := strings.Split(e, ":::")
+		val := strings.Split(e, "%%%")
+		fmt.Printf("%d: %v\n", len(val), val)
 		if len(val) == 2 {
 			target, prefix = val[0], val[1]
+		} else if len(val) == 1 {
+			target, prefix = "", val[0]
 		}
+		fmt.Println(target, prefix)
 		gp, err := CreatePrefix(prefix, target)
 		if err != nil {
 			t.Error(err)
 		}
 		if !reflect.DeepEqual(p, gp) {
-			t.Errorf("failed at elem: %s", e)
+			t.Errorf("failed at elem: %s: expecting %v, got %v", e, p, gp)
 		}
 
+	}
+}
+
+func TestParsePath(t *testing.T) {
+	for p, g := range pathsSet {
+		pg, err := ParsePath(p)
+		if err != nil {
+			t.Error(err)
+		}
+		if !reflect.DeepEqual(g, pg) {
+			t.Errorf("ParsePath failed with path: %s", p)
+		}
 	}
 }
