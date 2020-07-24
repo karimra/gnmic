a682a809c03a2036732cf54e9f910dfccd736466
Radhouani Karim <karim.radhouani@nokia.com>
2020-07-24 19:56:46 +0800

    add format printing to capabilities command

--- cmd/capabilities.go
+++ cmd/capabilities.go
@@ -16,6 +16,7 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"sync"
@@ -24,6 +25,7 @@ import (
 	"github.com/openconfig/gnmi/proto/gnmi"
 	"github.com/openconfig/gnmi/proto/gnmi_ext"
 	"github.com/spf13/viper"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/encoding/prototext"
 
 	"github.com/spf13/cobra"
@@ -85,21 +87,58 @@ func printCapResponse(r *gnmi.CapabilityResponse, address string) {
 	if len(addresses) > 1 && !viper.GetBool("no-prefix") {
 		printPrefix = fmt.Sprintf("[%s] ", address)
 	}
-	if viper.GetString("format") == "prototext" {
+	switch viper.GetString("format") {
+	case "protojson":
+		b, err := protojson.MarshalOptions{Multiline: true, Indent: "  "}.Marshal(r)
+		if err != nil {
+			logger.Printf("error marshaling protojson msg: %v", err)
+			if !viper.GetBool("log") {
+				fmt.Printf("error marshaling protojson msg: %v\n", err)
+			}
+			return
+		}
+		fmt.Printf("%s\n", indent(printPrefix, string(b)))
+		return
+	case "prototext":
 		fmt.Printf("%s\n", indent(printPrefix, prototext.Format(r)))
 		return
-	}
-	fmt.Printf("%sgNMI version: %s\n", printPrefix, r.GNMIVersion)
-	if viper.GetBool("version") {
-		return
-	}
-	fmt.Printf("%ssupported models:\n", printPrefix)
-	for _, sm := range r.SupportedModels {
-		fmt.Printf("%s  - %s, %s, %s\n", printPrefix, sm.GetName(), sm.GetOrganization(), sm.GetVersion())
-	}
-	fmt.Printf("%ssupported encodings:\n", printPrefix)
-	for _, se := range r.SupportedEncodings {
-		fmt.Printf("%s  - %s\n", printPrefix, se.String())
+	case "json":
+		capRspMsg := capResponse{}
+
+		capRspMsg.Version = r.GetGNMIVersion()
+		for _, sm := range r.SupportedModels {
+			capRspMsg.SupportedModels = append(capRspMsg.SupportedModels,
+				supportedModels{
+					Name:         sm.GetName(),
+					Organization: sm.GetOrganization(),
+					Version:      sm.GetVersion(),
+				})
+		}
+		for _, se := range r.SupportedEncodings {
+			capRspMsg.Encodings = append(capRspMsg.Encodings, se.String())
+		}
+		b, err := json.MarshalIndent(capRspMsg, printPrefix, "  ")
+		if err != nil {
+			logger.Printf("failed to marshal capabilities response: %v", err)
+			if !viper.GetBool("log") {
+				fmt.Printf("failed to marshal capabilities response: %v", err)
+				return
+			}
+		}
+		fmt.Println(string(b))
+	default:
+		fmt.Printf("%sgNMI version: %s\n", printPrefix, r.GNMIVersion)
+		if viper.GetBool("version") {
+			return
+		}
+		fmt.Printf("%ssupported models:\n", printPrefix)
+		for _, sm := range r.SupportedModels {
+			fmt.Printf("%s  - %s, %s, %s\n", printPrefix, sm.GetName(), sm.GetOrganization(), sm.GetVersion())
+		}
+		fmt.Printf("%ssupported encodings:\n", printPrefix)
+		for _, se := range r.SupportedEncodings {
+			fmt.Printf("%s  - %s\n", printPrefix, se.String())
+		}
 	}
 	fmt.Println()
 }
@@ -108,4 +147,15 @@ func init() {
 	rootCmd.AddCommand(capabilitiesCmd)
 	capabilitiesCmd.Flags().BoolVarP(&printVersion, "version", "", false, "show gnmi version only")
 	viper.BindPFlag("capabilities-version", capabilitiesCmd.LocalFlags().Lookup("version"))
+}
+
+type capResponse struct {
+	Version         string            `json:"version,omitempty"`
+	SupportedModels []supportedModels `json:"supported-models,omitempty"`
+	Encodings       []string          `json:"encodings,omitempty"`
+}
+type supportedModels struct {
+	Name         string `json:"name,omitempty"`
+	Organization string `json:"organization,omitempty"`
+	Version      string `json:"version,omitempty"`
 }
