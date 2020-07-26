4a9c48966959c21411a3d619362ae14b41ad4422
Radhouani Karim <karim.radhouani@nokia.com>
2020-07-27 00:01:52 +0800

    remove '/' as prefix of xpaths

--- collector/msg.go
+++ collector/msg.go
@@ -109,9 +109,6 @@ func gnmiPathToXPath(p *gnmi.Path) string {
 	}
 	elems := p.GetElem()
 	numElems := len(elems)
-	if numElems > 0 {
-		sb.WriteString("/")
-	}
 	for i, pe := range elems {
 		sb.WriteString(pe.GetName())
 		for k, v := range pe.GetKey() {
