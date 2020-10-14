e024c8e3181d39a8d740b0ec457c3c60f051f40d
Karim Radhouani <medkarimrdi@gmail.com>
2020-10-14 11:52:03 +0800

    register prometheus client output

--- outputs/all/all.go
+++ outputs/all/all.go
@@ -2,10 +2,11 @@ package all
 
 import (
 	_ "github.com/karimra/gnmic/outputs/file"
+	_ "github.com/karimra/gnmic/outputs/influxdb_output"
 	_ "github.com/karimra/gnmic/outputs/kafka_output"
 	_ "github.com/karimra/gnmic/outputs/nats_output"
+	_ "github.com/karimra/gnmic/outputs/prometheus_output"
 	_ "github.com/karimra/gnmic/outputs/stan_output"
 	_ "github.com/karimra/gnmic/outputs/tcp_output"
 	_ "github.com/karimra/gnmic/outputs/udp_output"
-	_ "github.com/karimra/gnmic/outputs/influxdb_output"
 )
