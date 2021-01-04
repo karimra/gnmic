0004a3b670d8dfb15545f54f366f9380b7d44012
Karim Radhouani <medkarimrdi@gmail.com>
2021-01-04 12:46:17 +0800

    validate processor types in config

--- config/processors.go
+++ config/processors.go
@@ -1,6 +1,10 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/karimra/gnmic/formatters"
+)
 
 func (c *Config) GetEventProcessors() (map[string]map[string]interface{}, error) {
 	eps := c.FileConfig.GetStringMap("processors")
@@ -8,9 +12,14 @@ func (c *Config) GetEventProcessors() (map[string]map[string]interface{}, error)
 	for name, epc := range eps {
 		switch epc := epc.(type) {
 		case map[string]interface{}:
+			c.logger.Printf("validating processor %q config", name)
+			err := c.validateProcessorConfig(epc)
+			if err != nil {
+				return nil, err
+			}
 			evpConfig[name] = epc
 		case nil:
-			return nil, nil
+			return nil, fmt.Errorf("empty processor %q config", name)
 		default:
 			c.logger.Printf("malformed processors config, %+v", epc)
 			return nil, fmt.Errorf("malformed processors config, got %T", epc)
@@ -20,4 +29,22 @@ func (c *Config) GetEventProcessors() (map[string]map[string]interface{}, error)
 		c.logger.Printf("processors: %+v", evpConfig)
 	}
 	return evpConfig, nil
+}
+
+func (c *Config) validateProcessorConfig(pcfg map[string]interface{}) error {
+	for epType := range pcfg {
+		if !strInlist(epType, formatters.EventProcessorTypes) {
+			return fmt.Errorf("unknown processors type: %s", epType)
+		}
+	}
+	return nil
+}
+
+func strInlist(s string, ls []string) bool {
+	for _, ss := range ls {
+		if ss == s {
+			return true
+		}
+	}
+	return false
 }
