42d3f4485c6402dd3f8be99fe3d77438eb5d784b
Karim Radhouani <medkarimrdi@gmail.com>
2021-03-05 19:35:59 +0800

    update convert tests

--- formatters/event_convert/event_convert_test.go
+++ formatters/event_convert/event_convert_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 type item struct {
-	input  *formatters.EventMsg
-	output *formatters.EventMsg
+	input  []*formatters.EventMsg
+	output []*formatters.EventMsg
 }
 
 var testset = map[string]struct {
@@ -29,65 +29,131 @@ var testset = map[string]struct {
 				output: nil,
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{}},
+				input:  make([]*formatters.EventMsg, 0),
+				output: make([]*formatters.EventMsg, 0),
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"name": 1}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"name": 1}},
-			},
-			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": "100"},
-					Tags:   map[string]string{"number": "name_tag"},
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"name": 1},
+					},
 				},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": int(100)},
-					Tags:   map[string]string{"number": "name_tag"},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"name": 1},
+					},
 				},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": int(100)},
-					Tags:   map[string]string{"number": "name_tag"},
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": "100"},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
 				},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": int(100)},
-					Tags:   map[string]string{"number": "name_tag"},
-				},
-			},
-			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": uint(100)},
-					Tags:   map[string]string{"number": "name_tag"},
-				},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": int(100)},
-					Tags:   map[string]string{"number": "name_tag"},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": int(100)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
 				},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": float64(100)},
-					Tags:   map[string]string{"number": "name_tag"},
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": "100"},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+					{
+						Values: map[string]interface{}{"number": "200"},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
 				},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": int(100)},
-					Tags:   map[string]string{"number": "name_tag"},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": int(100)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+					{
+						Values: map[string]interface{}{"number": int(200)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
 				},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": true},
-					Tags:   map[string]string{"number": "name_tag"},
+				input: []*formatters.EventMsg{
+					{
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+					{
+						Values: map[string]interface{}{"number": "200"},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
 				},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": true},
-					Tags:   map[string]string{"number": "name_tag"},
+				output: []*formatters.EventMsg{
+					{
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+					{
+						Values: map[string]interface{}{"number": int(200)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+				},
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": int(100)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": int(100)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+				},
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": uint(100)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": int(100)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+				},
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": float64(100)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": int(100)},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+				},
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": true},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": true},
+						Tags:   map[string]string{"number": "name_tag"},
+					},
 				},
 			},
 		},
@@ -104,108 +170,102 @@ var testset = map[string]struct {
 				output: nil,
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{}},
+				input:  []*formatters.EventMsg{{Values: map[string]interface{}{}}},
+				output: []*formatters.EventMsg{{Values: map[string]interface{}{}}},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"name_value_bytes": "42"}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"name_value_bytes": uint(42)}},
+				input: []*formatters.EventMsg{{
+					Values: map[string]interface{}{"name_value_bytes": "42"}}},
+				output: []*formatters.EventMsg{{
+					Values: map[string]interface{}{"name_value_bytes": uint(42)}}},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"name_value_bytes": uint(42)}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"name_value_bytes": uint(42)}},
+				input: []*formatters.EventMsg{{
+					Values: map[string]interface{}{"name_value_bytes": uint(42)}}},
+				output: []*formatters.EventMsg{{
+					Values: map[string]interface{}{"name_value_bytes": uint(42)}}},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"name_value_bytes": -42}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"name_value_bytes": uint(0)}},
+				input: []*formatters.EventMsg{{
+					Values: map[string]interface{}{"name_value_bytes": -42}}},
+				output: []*formatters.EventMsg{{
+					Values: map[string]interface{}{"name_value_bytes": uint(0)}}},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"name_value_bytes": true}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"name_value_bytes": true}},
+				input: []*formatters.EventMsg{{
+					Values: map[string]interface{}{"name_value_bytes": true}}},
+				output: []*formatters.EventMsg{{
+					Values: map[string]interface{}{"name_value_bytes": true}}},
 			},
 		},
 	},
 	"float_convert": {
 		processorType: processorType,
-		processor: map[string]interface{}{
-			"value-names": []string{"^number*"},
-			"type":        "float",
-		},
+		processor:     map[string]interface{}{"value-names": []string{"^number*"}, "type": "float"},
 		tests: []item{
 			{
 				input:  nil,
 				output: nil,
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{}},
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{},
+					},
+				},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": "1.1"}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": float64(1.1)}},
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": "1.1"},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": float64(1.1)},
+					},
+				},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": uint(42)}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": float64(42)}},
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": uint(42)},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": float64(42)},
+					},
+				},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": int(42)}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": float64(42)}},
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": int(42)},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": float64(42)},
+					},
+				},
 			},
 			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": true}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"number": true}},
-			},
-		},
-	},
-	"string_convert": {
-		processorType: processorType,
-		processor: map[string]interface{}{
-			"value-names": []string{"id"},
-			"type":        "string",
-		},
-		tests: []item{
-			{
-				input:  nil,
-				output: nil,
-			},
-			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{}},
-			},
-			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"id": 1}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"id": string("1")}},
-			},
-			{
-				input: &formatters.EventMsg{
-					Values: map[string]interface{}{"id": -1}},
-				output: &formatters.EventMsg{
-					Values: map[string]interface{}{"id": string("-1")}},
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": true},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": true},
+					},
+				},
 			},
 		},
 	},
@@ -225,29 +285,14 @@ func TestEventConvertToUint(t *testing.T) {
 		for i, item := range ts.tests {
 			t.Run("uint_convert", func(t *testing.T) {
 				t.Logf("running test item %d", i)
-				var inputMsg *formatters.EventMsg
-				if item.input != nil {
-					inputMsg = &formatters.EventMsg{
-						Name:      item.input.Name,
-						Timestamp: item.input.Timestamp,
-						Tags:      make(map[string]string),
-						Values:    make(map[string]interface{}),
-						Deletes:   item.input.Deletes,
+				outs := p.Apply(item.input...)
+				for j := range outs {
+					if !reflect.DeepEqual(outs[j], item.output[j]) {
+						t.Logf("failed at uint_convert item %d, index %d", i, j)
+						t.Logf("expected: %#v", item.output[j])
+						t.Logf("     got: %#v", outs[j])
+						t.Fail()
 					}
-					for k, v := range item.input.Tags {
-						inputMsg.Tags[k] = v
-					}
-					for k, v := range item.input.Values {
-						inputMsg.Values[k] = v
-					}
-				}
-				p.Apply(item.input)
-				t.Logf("input: %+v, changed: %+v", inputMsg, item.input)
-				if !reflect.DeepEqual(item.input, item.output) {
-					t.Logf("failed at uint_convert item %d", i)
-					t.Logf("expected: %#v", item.output)
-					t.Logf("     got: %#v", item.input)
-					t.Fail()
 				}
 			})
 		}
@@ -267,29 +312,14 @@ func TestEventConvertToInt(t *testing.T) {
 		for i, item := range ts.tests {
 			t.Run("int_convert", func(t *testing.T) {
 				t.Logf("running test item %d", i)
-				var inputMsg *formatters.EventMsg
-				if item.input != nil {
-					inputMsg = &formatters.EventMsg{
-						Name:      item.input.Name,
-						Timestamp: item.input.Timestamp,
-						Tags:      make(map[string]string),
-						Values:    make(map[string]interface{}),
-						Deletes:   item.input.Deletes,
+				outs := p.Apply(item.input...)
+				for j := range outs {
+					if !reflect.DeepEqual(outs[j], item.output[j]) {
+						t.Logf("failed at int_convert item %d, index %d", i, j)
+						t.Logf("expected: %#v", item.output[j])
+						t.Logf("     got: %#v", outs[j])
+						t.Fail()
 					}
-					for k, v := range item.input.Tags {
-						inputMsg.Tags[k] = v
-					}
-					for k, v := range item.input.Values {
-						inputMsg.Values[k] = v
-					}
-				}
-				p.Apply(item.input)
-				t.Logf("input: %+v, changed: %+v", inputMsg, item.input)
-				if !reflect.DeepEqual(item.input, item.output) {
-					t.Logf("failed at int_convert item %d", i)
-					t.Logf("expected: %#v", item.output)
-					t.Logf("     got: %#v", item.input)
-					t.Fail()
 				}
 			})
 		}
@@ -309,29 +339,14 @@ func TestEventConvertToString(t *testing.T) {
 		for i, item := range ts.tests {
 			t.Run("string_convert", func(t *testing.T) {
 				t.Logf("running test item %d", i)
-				var inputMsg *formatters.EventMsg
-				if item.input != nil {
-					inputMsg = &formatters.EventMsg{
-						Name:      item.input.Name,
-						Timestamp: item.input.Timestamp,
-						Tags:      make(map[string]string),
-						Values:    make(map[string]interface{}),
-						Deletes:   item.input.Deletes,
+				outs := p.Apply(item.input...)
+				for j := range outs {
+					if !reflect.DeepEqual(outs[j], item.output[j]) {
+						t.Logf("failed at string_convert item %d, index %d", i, j)
+						t.Logf("expected: %#v", item.output[j])
+						t.Logf("     got: %#v", outs[j])
+						t.Fail()
 					}
-					for k, v := range item.input.Tags {
-						inputMsg.Tags[k] = v
-					}
-					for k, v := range item.input.Values {
-						inputMsg.Values[k] = v
-					}
-				}
-				p.Apply(item.input)
-				t.Logf("input: %+v, changed: %+v", inputMsg, item.input)
-				if !reflect.DeepEqual(item.input, item.output) {
-					t.Logf("failed at string_convert item %d", i)
-					t.Logf("expected: %#v", item.output)
-					t.Logf("     got: %#v", item.input)
-					t.Fail()
 				}
 			})
 		}
@@ -351,29 +366,14 @@ func TestEventConvertToFloat(t *testing.T) {
 		for i, item := range ts.tests {
 			t.Run("float_convert", func(t *testing.T) {
 				t.Logf("running test item %d", i)
-				var inputMsg *formatters.EventMsg
-				if item.input != nil {
-					inputMsg = &formatters.EventMsg{
-						Name:      item.input.Name,
-						Timestamp: item.input.Timestamp,
-						Tags:      make(map[string]string),
-						Values:    make(map[string]interface{}),
-						Deletes:   item.input.Deletes,
+				outs := p.Apply(item.input...)
+				for j := range outs {
+					if !reflect.DeepEqual(outs[j], item.output[j]) {
+						t.Logf("failed at float_convert item %d, index %d", i, j)
+						t.Logf("expected: %#v", item.output[j])
+						t.Logf("     got: %#v", outs[j])
+						t.Fail()
 					}
-					for k, v := range item.input.Tags {
-						inputMsg.Tags[k] = v
-					}
-					for k, v := range item.input.Values {
-						inputMsg.Values[k] = v
-					}
-				}
-				p.Apply(item.input)
-				t.Logf("input: %+v, changed: %+v", inputMsg, item.input)
-				if !reflect.DeepEqual(item.input, item.output) {
-					t.Logf("failed at float_convert item %d", i)
-					t.Logf("expected: %#v", item.output)
-					t.Logf("     got: %#v", item.input)
-					t.Fail()
 				}
 			})
 		}
