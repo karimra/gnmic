40479045ffac4eb1004b4f5d60ac1d06a0b1dc22
Karim Radhouani <medkarimrdi@gmail.com>
2021-04-09 02:29:15 +0800

    add event allow processor

---
+++ formatters/event_allow/event_allow.go
@@ -0,0 +1,169 @@
+package event_allow
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/itchyny/gojq"
+	"github.com/karimra/gnmic/formatters"
+)
+
+const (
+	processorType = "event-allow"
+	loggingPrefix = "[" + processorType + "] "
+)
+
+// Allow Allows the msg if ANY of the Tags or Values regexes are matched
+type Allow struct {
+	formatters.EventProcessor
+	Condition  string   `mapstructure:"condition,omitempty"`
+	TagNames   []string `mapstructure:"tag-names,omitempty" json:"tag-names,omitempty"`
+	ValueNames []string `mapstructure:"value-names,omitempty" json:"value-names,omitempty"`
+	Tags       []string `mapstructure:"tags,omitempty" json:"tags,omitempty"`
+	Values     []string `mapstructure:"values,omitempty" json:"values,omitempty"`
+	Debug      bool     `mapstructure:"debug,omitempty" json:"debug,omitempty"`
+
+	tagNames   []*regexp.Regexp
+	valueNames []*regexp.Regexp
+	tags       []*regexp.Regexp
+	values     []*regexp.Regexp
+	code       *gojq.Code
+	logger     *log.Logger
+}
+
+func init() {
+	formatters.Register(processorType, func() formatters.EventProcessor {
+		return &Allow{
+			logger: log.New(ioutil.Discard, "", 0),
+		}
+	})
+}
+
+func (d *Allow) Init(cfg interface{}, opts ...formatters.Option) error {
+	err := formatters.DecodeConfig(cfg, d)
+	if err != nil {
+		return err
+	}
+	for _, opt := range opts {
+		opt(d)
+	}
+	d.Condition = strings.TrimSpace(d.Condition)
+	q, err := gojq.Parse(d.Condition)
+	if err != nil {
+		return err
+	}
+	d.code, err = gojq.Compile(q)
+	if err != nil {
+		return err
+	}
+	// init tag keys regex
+	d.tagNames = make([]*regexp.Regexp, 0, len(d.TagNames))
+	for _, reg := range d.TagNames {
+		re, err := regexp.Compile(reg)
+		if err != nil {
+			return err
+		}
+		d.tagNames = append(d.tagNames, re)
+	}
+	d.tags = make([]*regexp.Regexp, 0, len(d.Tags))
+	for _, reg := range d.Tags {
+		re, err := regexp.Compile(reg)
+		if err != nil {
+			return err
+		}
+		d.tags = append(d.tags, re)
+	}
+	//
+	d.valueNames = make([]*regexp.Regexp, 0, len(d.ValueNames))
+	for _, reg := range d.ValueNames {
+		re, err := regexp.Compile(reg)
+		if err != nil {
+			return err
+		}
+		d.valueNames = append(d.valueNames, re)
+	}
+
+	d.values = make([]*regexp.Regexp, 0, len(d.values))
+	for _, reg := range d.Values {
+		re, err := regexp.Compile(reg)
+		if err != nil {
+			return err
+		}
+		d.values = append(d.values, re)
+	}
+	if d.logger.Writer() != ioutil.Discard {
+		b, err := json.Marshal(d)
+		if err != nil {
+			d.logger.Printf("initialized processor '%s': %+v", processorType, d)
+			return nil
+		}
+		d.logger.Printf("initialized processor '%s': %s", processorType, string(b))
+	}
+	return nil
+}
+
+func (d *Allow) Apply(es ...*formatters.EventMsg) []*formatters.EventMsg {
+	for _, e := range es {
+		if e == nil {
+			continue
+		}
+		if d.Condition != "" {
+			ok, err := formatters.CheckCondition(d.code, e)
+			if err != nil {
+				d.logger.Printf("condition check failed: %v", err)
+				continue
+			}
+			if !ok {
+				*e = formatters.EventMsg{}
+				continue
+			}
+		}
+		for k, v := range e.Values {
+			for _, re := range d.valueNames {
+				if !re.MatchString(k) {
+					d.logger.Printf("value name '%s' matched regex '%s'", k, re.String())
+					*e = formatters.EventMsg{}
+					break
+				}
+			}
+			for _, re := range d.values {
+				if vs, ok := v.(string); ok {
+					if !re.MatchString(vs) {
+						d.logger.Printf("value '%s' matched regex '%s'", v, re.String())
+						*e = formatters.EventMsg{}
+						break
+					}
+				}
+			}
+		}
+		for k, v := range e.Tags {
+			for _, re := range d.tagNames {
+				if !re.MatchString(k) {
+					d.logger.Printf("tag name '%s' matched regex '%s'", k, re.String())
+					*e = formatters.EventMsg{}
+					break
+				}
+			}
+			for _, re := range d.tags {
+				if re.MatchString(v) {
+					d.logger.Printf("tag '%s' matched regex '%s'", v, re.String())
+					*e = formatters.EventMsg{}
+					break
+				}
+			}
+		}
+	}
+	return es
+}
+
+func (d *Allow) WithLogger(l *log.Logger) {
+	if d.Debug && l != nil {
+		d.logger = log.New(l.Writer(), loggingPrefix, l.Flags())
+	} else if d.Debug {
+		d.logger = log.New(os.Stderr, loggingPrefix, log.LstdFlags|log.Lmicroseconds)
+	}
+}
---
+++ formatters/event_allow/event_allow_test.go
@@ -0,0 +1,157 @@
+package event_allow
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/karimra/gnmic/formatters"
+)
+
+type item struct {
+	input  []*formatters.EventMsg
+	output []*formatters.EventMsg
+}
+
+var testset = map[string]struct {
+	processorType string
+	processor     map[string]interface{}
+	tests         []item
+}{
+	"allow_condition": {
+		processorType: processorType,
+		processor: map[string]interface{}{
+			"condition": ".values.value == 1",
+		},
+		tests: []item{
+			{
+				input:  nil,
+				output: nil,
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{}},
+				},
+				output: []*formatters.EventMsg{
+					{},
+				},
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"value": 1},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"value": 1},
+					},
+				},
+			},
+		},
+	},
+	"allow_values": {
+		processorType: processorType,
+		processor: map[string]interface{}{
+			"value-names": []string{"^number$"},
+		},
+		tests: []item{
+			{
+				input:  nil,
+				output: nil,
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{}},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{}},
+				},
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": 1},
+					},
+					{
+						Values: map[string]interface{}{"not-number": 1},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Values: map[string]interface{}{"number": 1},
+					},
+					{},
+				},
+			},
+		},
+	},
+	"allow_tags": {
+		processorType: processorType,
+		processor: map[string]interface{}{
+			"tag-names": []string{"^name*"},
+		},
+		tests: []item{
+			{
+				input:  nil,
+				output: nil,
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Tags: map[string]string{}},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Tags: map[string]string{}},
+				},
+			},
+			{
+				input: []*formatters.EventMsg{
+					{
+						Tags: map[string]string{"name": "dummy"},
+					},
+					{
+						Tags: map[string]string{"not-name": "dummy"},
+					},
+				},
+				output: []*formatters.EventMsg{
+					{
+						Tags: map[string]string{"name": "dummy"},
+					},
+					{},
+				},
+			},
+		},
+	},
+}
+
+func TestEventAllow(t *testing.T) {
+	for name, ts := range testset {
+		if pi, ok := formatters.EventProcessors[ts.processorType]; ok {
+			p := pi()
+			err := p.Init(ts.processor)
+			if err != nil {
+				t.Errorf("failed to initialize processors: %v", err)
+				return
+			}
+			t.Logf("processor: %+v", p)
+			for i, item := range ts.tests {
+				t.Run(name, func(t *testing.T) {
+					t.Logf("running test item %d", i)
+					outs := p.Apply(item.input...)
+					for j := range outs {
+						if !reflect.DeepEqual(outs[j], item.output[j]) {
+							t.Logf("failed at event allow, item %d, index %d", i, j)
+							t.Logf("expected: %#v", item.output[j])
+							t.Logf("     got: %#v", outs[j])
+							t.Fail()
+						}
+					}
+				})
+			}
+		}
+	}
+}
