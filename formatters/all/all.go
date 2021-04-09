1ac79c430099890248e3c808ad73d7b76b7a80c2
Karim Radhouani <medkarimrdi@gmail.com>
2021-04-09 19:40:38 +0800

    add missing import

--- formatters/all/all.go
+++ formatters/all/all.go
@@ -2,15 +2,16 @@ package all
 
 import (
 	_ "github.com/karimra/gnmic/formatters/event_add_tag"
+	_ "github.com/karimra/gnmic/formatters/event_allow"
 	_ "github.com/karimra/gnmic/formatters/event_convert"
 	_ "github.com/karimra/gnmic/formatters/event_date_string"
 	_ "github.com/karimra/gnmic/formatters/event_delete"
 	_ "github.com/karimra/gnmic/formatters/event_drop"
+	_ "github.com/karimra/gnmic/formatters/event_extract_tags"
 	_ "github.com/karimra/gnmic/formatters/event_jq"
 	_ "github.com/karimra/gnmic/formatters/event_merge"
 	_ "github.com/karimra/gnmic/formatters/event_strings"
 	_ "github.com/karimra/gnmic/formatters/event_to_tag"
 	_ "github.com/karimra/gnmic/formatters/event_trigger"
 	_ "github.com/karimra/gnmic/formatters/event_write"
-	_ "github.com/karimra/gnmic/formatters/event_extract_tags"
 )
