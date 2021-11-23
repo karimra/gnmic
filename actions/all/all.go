f1f6ce7237ff2717ddc16a3bb8dcf6cc5e27ea54
Karim Radhouani <medkarimrdi@gmail.com>
2021-11-22 21:57:48 -0800

    add script action

--- actions/all/all.go
+++ actions/all/all.go
@@ -3,5 +3,6 @@ package all
 import (
 	_ "github.com/karimra/gnmic/actions/gnmi_action"
 	_ "github.com/karimra/gnmic/actions/http_action"
+	_ "github.com/karimra/gnmic/actions/script_action"
 	_ "github.com/karimra/gnmic/actions/template_action"
 )
---
+++ actions/script_action/script_action.go
@@ -0,0 +1,104 @@
+package script_action
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/karimra/gnmic/actions"
+	"github.com/karimra/gnmic/formatters"
+)
+
+const (
+	loggingPrefix   = "[script_action] "
+	actionType      = "script"
+	defaultTemplate = "{{ . }}"
+	defaultShell    = "/bin/bash"
+)
+
+func init() {
+	actions.Register(actionType, func() actions.Action {
+		return &scriptAction{
+			logger: log.New(io.Discard, "", 0),
+		}
+	})
+}
+
+type scriptAction struct {
+	Name    string `mapstructure:"name,omitempty"`
+	Shell   string `mapstructure:"shell,omitempty"`
+	Command string `mapstructure:"command,omitempty"`
+	File    string `mapstructure:"file,omitempty"`
+	Debug   bool   `mapstructure:"debug,omitempty"`
+
+	logger *log.Logger
+}
+
+func (s *scriptAction) Init(cfg map[string]interface{}, opts ...actions.Option) error {
+	err := actions.DecodeConfig(cfg, s)
+	if err != nil {
+		return err
+	}
+
+	for _, opt := range opts {
+		opt(s)
+	}
+	if s.Name == "" {
+		return fmt.Errorf("action type %q missing name field", actionType)
+	}
+	err = s.setDefaults()
+	if err != nil {
+		return err
+	}
+	s.logger.Printf("action name %q of type %q initialized: %v", s.Name, actionType, s)
+	return nil
+}
+
+func (s *scriptAction) Run(e *formatters.EventMsg, env, vars map[string]interface{}) (interface{}, error) {
+	if s.Command == "" && s.File == "" {
+		return nil, nil
+	}
+	stdout := new(bytes.Buffer)
+	stderr := new(bytes.Buffer)
+	var cmd *exec.Cmd
+	if s.Command != "" {
+		cmds := strings.Split(s.Command, "\n")
+		args := append([]string{"-c"}, strings.Join(cmds, "; "))
+		cmd = exec.Command(s.Shell, args...)
+	}
+	if s.File != "" {
+		cmd = exec.Command(s.File)
+	}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	cmd.Env = os.Environ()
+	for k, v := range env {
+		k = strings.ReplaceAll(k, "-", "_")
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, v))
+	}
+	for k, v := range vars {
+		k = strings.ReplaceAll(k, "-", "_")
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, v))
+	}
+	err := cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("%v: %s", err, stderr.String())
+	}
+	if stderr.String() != "" {
+		return map[string]string{"stderr": stderr.String()}, nil
+	}
+	return map[string]string{"stdout": stdout.String()}, nil
+}
+
+func (s *scriptAction) NName() string { return s.Name }
+
+func (s *scriptAction) setDefaults() error {
+	if s.Shell == "" {
+		s.Shell = defaultShell
+	}
+	return nil
+}
