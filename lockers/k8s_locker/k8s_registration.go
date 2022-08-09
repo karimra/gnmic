0fdea869cafe6bc2db03f37d37276bb71c06f5b3
Erick Turnquist <jhujhiti@adjectivism.org>
2022-08-09 13:05:54 -0400

    go fmt

--- lockers/k8s_locker/k8s_registration.go
+++ lockers/k8s_locker/k8s_registration.go
@@ -49,9 +49,9 @@ func (k *k8sLocker) WatchServices(ctx context.Context, serviceName string, tags 
 func (k *k8sLocker) watch(ctx context.Context, serviceName string, tags []string, sChan chan<- []*lockers.Service, watchTimeout time.Duration, resourceVersion string) (string, error) {
 	timeoutSeconds := int64(watchTimeout.Seconds())
 	listopts := metav1.ListOptions{
-		FieldSelector:        fields.OneTermEqualSelector(metav1.ObjectNameField, serviceName).String(),
-		ResourceVersion:      resourceVersion,
-		TimeoutSeconds:       &timeoutSeconds,
+		FieldSelector:   fields.OneTermEqualSelector(metav1.ObjectNameField, serviceName).String(),
+		ResourceVersion: resourceVersion,
+		TimeoutSeconds:  &timeoutSeconds,
 	}
 	if k.Cfg.Debug {
 		if resourceVersion == "" {
